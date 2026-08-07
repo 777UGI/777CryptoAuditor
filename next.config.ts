8a0a6e8dff2f97efebf7bf80b86b5fca01647bed
chiklii <workforskullgod@gmail.com>
2026-08-07 23:24:48 +0530

    Fix build error

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["tronweb"],
-  // @ts-expect-error Next.js 15 config
   allowedDevOrigins: ["10.138.143.251", "pink-bars-notice.loca.lt", "loca.lt"],
 };
 
